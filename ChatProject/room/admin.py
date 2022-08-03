f1ab532a4690f252b6f34f87ba72e41966d29f18
vazgenmkrtchyan001 <vazgenmkrtchyan001@gmail.com>
2022-08-03 16:00:02 +0400

    Chat

--- ChatProject/room/admin.py
+++ ChatProject/room/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Room, Message
 
 
-admin.site.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Room, RoomAdmin)
 admin.site.register(Message)
--- ChatProject/room/models.py
+++ ChatProject/room/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Room(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'room'
+        verbose_name_plural = 'rooms'
 
 
 class Message(models.Model):
@@ -12,5 +20,10 @@ class Message(models.Model):
     content = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return str(self.room)
+
     class Meta:
+        verbose_name = 'message'
+        verbose_name_plural = 'messages'
         ordering = ('date_added',)
